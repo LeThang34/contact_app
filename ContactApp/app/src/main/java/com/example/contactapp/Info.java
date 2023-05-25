e07bb28addaf94178e792e679edd351b54dc3079
LeThang34 <104156515+LeThang34@users.noreply.github.com>
2023-05-25 08:45:29 +0700

    first commit

---
+++ ContactApp/app/src/main/java/com/example/contactapp/Info.java
@@ -0,0 +1,63 @@
+package com.example.contactapp;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class Info extends AppCompatActivity {
+    TextView txtname, txtnumber;
+    ImageView imgcall, imgmess, imginfo;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info_friend);
+        txtname = findViewById(R.id.txtname);
+        txtnumber = findViewById(R.id.txtnumber);
+        imgcall = findViewById(R.id.imgcall);
+        imgmess = findViewById(R.id.imgmess);
+        imginfo = findViewById(R.id.imginfo);
+        //Nhận intent
+        Intent intent = getIntent();
+        //Lấy Bundle khỏi intent
+        Bundle mybundle = intent.getBundleExtra("mybundle");
+        //Lấy dữ liệu khỏi bundle
+        String a = mybundle.getString("name");
+        String b = mybundle.getString("number");
+        txtname.setText(a+"");
+        txtnumber.setText(b+"");
+        imgcall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Khai báo intent ẩn
+                //Trong intent, dữ liệu đều được đổi thành chuẩn uri
+                Intent intentcall = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+b));
+                //Yêu cầu sự đồng ý của người dùng
+                if (ActivityCompat.checkSelfPermission(Info.this, Manifest.permission.CALL_PHONE) !=
+                        PackageManager.PERMISSION_GRANTED)
+                {
+                    ActivityCompat.requestPermissions(Info.this, new String[]{Manifest.permission.CALL_PHONE}
+                            ,1);
+                    return;
+                }
+                startActivity(intentcall);
+            }
+        });
+        imgmess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Khai báo intent ẩn gọi đến ứng dụng tin nhắn
+                Intent intentsms = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:"+b));
+                startActivity(intentsms);
+            }
+        });
+    }
+}
---
+++ ContactApp/app/src/main/java/com/example/contactapp/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.contactapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    String DB_PATH_SUFFIX = "/databases/"; //Thư mục chứa csdl trong đt <Mặc định>
+    SQLiteDatabase database=null;//Tên csdl
+    String DATABASE_NAME="contact.db"; //Tên file csdl
+    //Khai báo listView
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Tạo các tham số cho ListView
+        lv = findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, mylist);
+        lv.setAdapter(myadapter);
+
+        processCopy();//Gọi hàm cop csdl từ thư mục assets vào thư mục database trong bộ nhớ đt
+        // Truy vấn CSDL và cập nhật hiển thị lên Listview
+        database=openOrCreateDatabase("contact.db",MODE_PRIVATE, null);
+        Cursor c = database.query("dbcontact", null,
+                null, null,null, null, null);
+        String data = "";
+        c.moveToFirst();//Di chuyển con trỏ về bản ghi đầu tiên
+        while (c.isAfterLast() == false) // Nếu không phải là bản ghi cuối cùng
+        {
+            data = c.getString(1) + "\n" + c.getString(0);
+            mylist.add(data);
+            c.moveToNext();
+        }
+        c.close();
+        myadapter.notifyDataSetChanged();
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String) parent.getItemAtPosition(position);
+                String[] values = item.split("\n");
+                Intent intent = new Intent(MainActivity.this, Info.class);
+                //Lấy dữ liệu
+                String name = values[0];
+                String number = values[1];
+                //Đóng gói vào bundle
+                Bundle mybundle = new Bundle();//đối tượng dùng để chứa dữ liệu trước khi đưa vào intent
+                //Đưa dữ liệu vào Bundle
+                mybundle.putString("name", name);
+                mybundle.putString("number", number);
+                //Đưa Bundle vào Intent
+                intent.putExtra("mybundle", mybundle);
+                //Khởi động intent
+                startActivity(intent);
+            }
+        });
+    }
+
+    //Hàm copy csdl Sqlite từ thư mục assets vào thư mục cài đặt ứng dụng
+    private void processCopy() {
+        File dbFile = getDatabasePath(DATABASE_NAME);
+        if (!dbFile.exists())
+        {
+            try{CopyDataBaseFromAsset();
+                Toast.makeText(this, "Copying sucess from Assets folder", Toast.LENGTH_LONG).show();
+            }
+            catch (Exception e){
+                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+    private String getDatabasePath() {
+        return getApplicationInfo().dataDir + DB_PATH_SUFFIX+ DATABASE_NAME;
+    }
+
+    public void CopyDataBaseFromAsset() {
+
+        try {
+            InputStream myInput;
+            myInput = getAssets().open(DATABASE_NAME);
+
+            String outFileName = getDatabasePath();
+
+            File f = new File(getApplicationInfo().dataDir + DB_PATH_SUFFIX);
+            if (!f.exists())
+                f.mkdir();
+
+            OutputStream myOutput = new FileOutputStream(outFileName);
+
+            int size = myInput.available();
+            byte[] buffer = new byte[size];
+            myInput.read(buffer);
+            myOutput.write(buffer);
+
+            myOutput.flush();
+            myOutput.close();
+            myInput.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+}
